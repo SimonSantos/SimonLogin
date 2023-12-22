fc4f09ffa18a5c7a03ebcd7532498f01f1a14619
Simon <simonsantos46842@gmail.com>
2023-12-22 15:57:34 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleForm() {
+    var loginContainer = document.getElementById('loginContainer');
+    var signupContainer = document.getElementById('signupContainer');
+
+    if (loginContainer.style.display === 'none') {
+        loginContainer.style.display = 'block';
+        signupContainer.style.display = 'none';
+    } else {
+        loginContainer.style.display = 'none';
+        signupContainer.style.display = 'block';
+    }
+}
